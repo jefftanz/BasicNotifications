c566c4c88b6e22fe9a9f2fcda8a84e3d803cd52b
jefftanz <jeff.tansey@gmail.com>
2015-07-13 18:36:14 -0500

    test 1

--- Application/src/main/java/com/example/android/basicnotifications/MainActivity.java
+++ Application/src/main/java/com/example/android/basicnotifications/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sample_layout);
-
+        String test = "";
     }
 
     /**
